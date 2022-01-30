77896d2ed1ba1a19968c05901b53ea49a6e3706b
Marcos Hernanz <marcosh.craft@gmail.com>
2022-01-30 12:48:47 +0100

    Display entry pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from . import util
+from markdown import markdown
 
 
 def index(request):
@@ -8,3 +8,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry = util.get_entry(title)
+    print(entry)
+    if entry is None:
+        return render(request, "encyclopedia/EntryNotFound.html", {
+            "title": title
+    })
+    else:
+        entry = markdown(entry)
+        print(entry)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title.capitalize(), "entry": entry
+    })
