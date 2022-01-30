5ad7ea582b0a1f85058cb604dd349089069d5d3e
Marcos Hernanz <marcosh.craft@gmail.com>
2022-01-30 17:17:00 +0100

    Search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from . import util
 from markdown import markdown
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "searching": False
     })
 
 def entry(request, title):
     entry = util.get_entry(title)
-    print(entry)
     if entry is None:
         return render(request, "encyclopedia/EntryNotFound.html", {
             "title": title
@@ -21,3 +22,19 @@ def entry(request, title):
         return render(request, "encyclopedia/entry.html", {
             "title": title.capitalize(), "entry": entry
     })
+
+def search(request):
+    search = request.GET.get("q","")
+    entry = util.get_entry(search)
+    if entry is None:
+        entry_titles = []
+        for title in util.list_entries():
+            if search.lower() in title.lower():
+                entry_titles.append(title)
+        return render(request, "encyclopedia/index.html", {
+            "entries": entry_titles,
+            "searching": True,
+            "value": search
+        })
+    else:
+        return HttpResponseRedirect(reverse("entry", kwargs={"title": search }))
