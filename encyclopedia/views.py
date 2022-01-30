a1f7f7002a84effcc8e4a864d71c0280a9d1eac3
Marcos Hernanz <marcosh.craft@gmail.com>
2022-01-30 17:49:32 +0100

    Started newPage

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,6 +3,14 @@ from . import util
 from markdown import markdown
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django import forms
+
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title", widget=forms.TextInput(attrs={"class": "form-control col-md-8 col-lg-8"}))
+    content = forms.CharField(label="Content", widget=forms.Textarea(attrs={"class": "form-control col-md-8 col-lg-8"}))
+    edit = forms.BooleanField(initial=False, widget=forms.HiddenInput(), required=False)
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -10,10 +18,11 @@ def index(request):
         "searching": False
     })
 
+
 def entry(request, title):
     entry = util.get_entry(title)
     if entry is None:
-        return render(request, "encyclopedia/EntryNotFound.html", {
+        return render(request, "encyclopedia/entryNotFound.html", {
             "title": title
     })
     else:
@@ -22,6 +31,18 @@ def entry(request, title):
         return render(request, "encyclopedia/entry.html", {
             "title": title.capitalize(), "entry": entry
     })
+
+
+def newEntry(request):
+    if request.method == "POST":
+        pass
+
+    else:
+        return render(request, "encyclopedia/newEntry.html", {
+            "form": NewEntryForm,
+            "existing": False
+        })
+
 
 def search(request):
     search = request.GET.get("q","")
